857324dd5236ee313918d4a3029223bfa7dc8073
tania_rocha <tania.rocha@mobbit.com>
2015-09-26 23:46:12 +0100

    Start awards view

--- mobile-app/js/index.js
+++ mobile-app/js/index.js
@@ -40,20 +40,21 @@ $('.menu-container').on('click', function(){
   console.log(id);
   switch(id){
     case 'menu-stores':
-      $('#view-stores').show();
-      methods.showAllStores(storesData, $('.stores-container'), categoriesData);
+      $('#view-stores').show(); $('#tab').animate({marginLeft: '40%'}, 100);
+      methods.showAllStores(storesData, $('.stores-container'), categoriesData, awardsData);
       break;
     case 'menu-qrcode':
-      $('#view-qrcode').show();
+      $('#view-qrcode').show(); $('#tab').animate({marginLeft: '0%'}, 100);
       break;
     case 'menu-awards':
-      $('#view-awards').show();
+      $('#view-awards').show(); $('#tab').animate({marginLeft: '20%'}, 100);
+      methods.showAllAwards(awardsData, storesData, $('.awards-container'));
       break;
     case 'menu-favorites':
-      $('#view-favorites').show();
+      $('#view-favorites').show(); $('#tab').animate({marginLeft: '60%'}, 100);
       break;
     case 'menu-settings':
-      $('#view-settings').show();
+      $('#view-settings').show(); $('#tab').animate({marginLeft: '80%'}, 100);
       break;
   }
 });
@@ -64,35 +65,60 @@ $('.view-menu').on('click', function(){
   $(this).addClass('selected');
   switch($(this).attr('id')){
     case 'stores-all':
-      $('.categories-container').removeClass('selected'); $('.stores-container').addClass('selected');
-      methods.showAllStores(storesData, $('.stores-container'), categoriesData);
+      $('.categories-container').removeClass('selected'); $('.locations-container').removeClass('selected'); $('.stores-container').addClass('selected');
+      methods.showAllStores(storesData, $('.stores-container'), categoriesData, awardsData);
       break;
     case 'stores-categories':
-      $('.stores-container').removeClass('selected'); $('.categories-container').addClass('selected');
+      $('.stores-container').removeClass('selected'); $('.locations-container').removeClass('selected'); $('.categories-container').addClass('selected');
       methods.showCategories(categoriesData, $('.categories-container'));
+      break;
+    case 'stores-locations':
+      $('.categories-container').removeClass('selected'); $('.stores-container').removeClass('selected'); $('.locations-container').addClass('selected');
+      methods.showLocations(locationsData, $('.locations-container'));
       break;
   }
 });
 
-var categoriesData = [
-  {id: '1', name: 'Restauração'},
-  {id: '2', name: 'Vestuário'},
-  {id: '3', name: 'Perfumaria'},
-  {id: '4', name: 'Acessórios'},
-  {id: '5', name: 'Sapatarias'}
+var awardsData = [
+  {id: 1, title: "Vale uma sandes á escolha", description: "Na compra de qualquer salada, o Vitaminas ofereçe uma sandes á sua companhia.", store: 1},
+  {id: 2, title: "50% na segunda compra", description: "50% a partir do segundo artigo comprado", store: 2},
+  {id: 3, title: "2 dias de 50% de desconto", description: "Você escolhe os 2 dias em que vai usufruir do seu desconto.", store: 3},
+  {id: 4, title: "Vale uma refeição para duas pessoas", description: "Traga a sua companhia e usufrua desta refeição que lhe estamos a oferecer.", store: 1},
+  {id: 5, title: "5% de desconto nas suas proximas 5 compras", description: "Queremos voltar a vê-lo em breve, para isso oferecemos-lhe 5% de desconto nas suas proximas 5 compras.", store: 5},
+  {id: 6, title: "Desconto de 30€", description: "Desconto de 30€ em qualquer compra superior a 50€. Dispõe de 10 dias para usufruir do seu prémio", store: 2},
+  {id: 7, title: "Vale 10€", description: "Desconto de 10€ em qualquer compra superior a 30€. Dispõe de 10 dias para usufruir do seu prémio", store: 10},
+  {id: 8, title: "Desconto de 20€", description: "Desconto de 20€ em qualquer compra superior a 50€. Dispõe de 10 dias para usufruir do seu prémio", store: 7}
 ];
 
-var storesData = {
-  "stores": [
-    {"id" : "1", name: "Vitaminas", categorie: "1", favorite: false, image: "assets/vitaminas.jpg", decription: "Descrição breve da oferta"},
-    {"id" : "2", name: "Primark", categorie: "1", favorite: false, image: "assets/vitaminas.jpg", decription: "Descrição breve da oferta"},
-    {"id" : "3", name: "Berska", categorie: "2", favorite: false, image: "assets/vitaminas.jpg", decription: "Descrição breve da oferta"},
-    {"id" : "4", name: "H3", categorie: "1", favorite: false, image: "assets/vitaminas.jpg", decription: "Descrição breve da oferta"},
-    {"id" : "5", name: "Gardenia", categorie: "5", favorite: false, image: "assets/vitaminas.jpg", decription: "Descrição breve da oferta"},
-    {"id" : "6", name: "Urban Beach", categorie: "2", favorite: false, image: "assets/vitaminas.jpg", decription: "Descrição breve da oferta"},
-    {"id" : "7", name: "Mr. Blue", categorie: "2", favorite: false, image: "assets/vitaminas.jpg", decription: "Descrição breve da oferta"},
-    {"id" : "8", name: "Pizza Hut", categorie: "1", favorite: false, image: "assets/vitaminas.jpg", decription: "Descrição breve da oferta"},
-    {"id" : "9", name: "Pinkie", categorie: "2", favorite: false, image: "assets/vitaminas.jpg", decription: "Descrição breve da oferta"},
-    {"id" : "10", name: "Perfumes & Companhia", categorie: "3", favorite: false, image: "assets/vitaminas.jpg", decription: "Descrição breve da oferta"}
-  ]
-};
+var storesData = [
+  {id : 1, name: "Vitaminas", categorie: 1, favorite: false, localidade: 1, image: "assets/vitaminas.jpg"},
+  {id : 2, name: "Primark", categorie: 1, favorite: false, localidade: 2, image: "assets/vitaminas.jpg"},
+  {id : 3, name: "Berska", categorie: 2, favorite: false, localidade: 5, image: "assets/vitaminas.jpg"},
+  {id : 4, name: "H3", categorie: 1, favorite: false, localidade: 4, image: "assets/vitaminas.jpg"},
+  {id : 5, name: "Gardenia", categorie: 5, favorite: false, localidade: 5, image: "assets/vitaminas.jpg"},
+  {id : 6, name: "Urban Beach", categorie: 2, favorite: false, localidade: 6, image: "assets/vitaminas.jpg"},
+  {id : 7, name: "Mr. Blue", categorie: 2, favorite: false, localidade: 7, image: "assets/vitaminas.jpg"},
+  {id : 8, name: "Pizza Hut", categorie: 1, favorite: false, localidade: 8, image: "assets/vitaminas.jpg"},
+  {id : 9, name: "Pinkie", categorie: 2, favorite: false, localidade: 9, image: "assets/vitaminas.jpg"},
+  {id : 10, name: "Perfumes & Companhia", categorie: 3, favorite: false, localidade: 9, image: "assets/vitaminas.jpg"}
+];
+
+var categoriesData = [
+  {id: 1, name: 'Restauração'},
+  {id: 2, name: 'Vestuário'},
+  {id: 3, name: 'Perfumaria'},
+  {id: 4, name: 'Acessórios'},
+  {id: 5, name: 'Sapatarias'}
+];
+
+var locationsData = [
+  {id: 1, name: 'Lisboa'},
+  {id: 2, name: 'Oeiras'},
+  {id: 3, name: 'Cascais'},
+  {id: 4, name: 'Setúbal'},
+  {id: 5, name: 'Leiria'},
+  {id: 6, name: 'Viana do Castelo'},
+  {id: 7, name: 'Porto'},
+  {id: 8, name: 'Vila do Conde'},
+  {id: 9, name: 'Régua'}
+];
--- mobile-app/js/storesMethods.js
+++ mobile-app/js/storesMethods.js
@@ -2,24 +2,63 @@ function storesMethods(){
 
 }
 
-storesMethods.prototype.showAllStores = function(data, container, categoriesData){
-  var stores = data.stores;
+storesMethods.prototype.showAllStores = function(data, container, categoriesData, awardsData){
   container.empty();
-  stores.forEach(function(store){
+  data.forEach(function(store){
+    var categoria = getCategorieNameById(store.categorie, categoriesData).toUpperCase();
+    var premio = getAwardByStore(store.id, awardsData);
     var storeDiv = $('<div class="store-container" id="store-'+store.id+'">'
       +'<div class="image" style="background-image: url('+store.image+')"></div>'
       +'<div class="body">'
         +'<div class="table-cell">'
           +'<div class="name">'+store.name.toUpperCase()+'</div>'
-          +'<div class="description">'+store.decription+'</div>'
-          +'<div class="categorie">'+getCategorieNameById(store.id, categoriesData).toUpperCase()+'</div>'
+          +'<div class="description">'+premio+'</div>'
+          +'<div class="categorie">'+categoria+'</div>'
         +'</div>'
       +'</div>'
       +'<div class="arrow"><span></span></div>'
-      +'</div>');
+    +'</div>');
     container.append(storeDiv);
   });
 };
+
+storesMethods.prototype.showAllAwards = function(data, stores, container){
+  container.empty();
+  data.forEach(function(award){
+    var store = getStoreById(award.store, stores);
+    var awardDiv = $('<div class="award-container" id="award-'+award.id+'">'
+      +'<div class="image" style="background-image: url('+store.image+')"></div>'
+      +'<div class="body">'
+        +'<div class="table-cell">'
+          +'<div class="store">'+store.name.toUpperCase()+'</div>'
+          +'<div class="description">'+award.title+'</div>'
+        +'</div>'
+      +'</div>'
+      +'<div class="arrow"><span></span></div>'
+    +'</div>');
+    container.append(awardDiv);
+  });
+}
+
+function getStoreById(storeId, data){
+  var storeInfo = null;
+  data.forEach(function(store){
+    if(store.id === storeId){
+      storeInfo = store; return storeInfo;
+    }
+  }); 
+  return storeInfo;
+}
+
+function getAwardByStore(id, awardsData){
+  var title="";
+  awardsData.forEach(function(award){
+    if(id === award.store){
+      title = award.title; return title;
+    }
+  });
+  return title;
+}
 
 function getCategorieNameById(id, categoriesData){
   var returnedCat = "";
@@ -34,7 +73,32 @@ function getCategorieNameById(id, categoriesData){
 storesMethods.prototype.showCategories = function(data, container){
   container.empty();
   data.forEach(function(categorie){
-    var categorieDiv = $('<div class="categorie-container" id="categorie-'+categorie.id+'"><span>'+categorie.name+'</span></div>');
+    var categorieDiv = $('<div class="categorie-container" id="categorie-'+categorie.id+'">'
+        +'<div class="name"><div class="table-cell">'+categorie.name.toUpperCase()+'</div></div>'
+        +'<div class="check-cat">'
+          +'<div class="table-cell">'
+            +'<div class="not-check"></div>'
+            +'<div class="check"></div>'
+          +'</div>'
+        +'</div>'
+      +'</div>');
     container.append(categorieDiv);
+  });
+};
+
+storesMethods.prototype.showLocations = function(data, container){
+  container.empty();
+  data.forEach(function(location){
+    var locationDiv = $('<div class="location-container" id="location-'+location.id+'">'
+        +'<div class="name"><div class="table-cell">'+location.name.toUpperCase()+'</div></div>'
+        +'<div class="check-loc">'
+          +'<div class="table-cell">'
+            +'<div class="not-check"></div>'
+            +'<div class="check"></div>'
+          +'</div>'
+        +'</div>'
+      +'</div>');
+    console.log(locationDiv);
+    container.append(locationDiv);
   });
 };
